021bb5df0388907fa33780acfed8703835b7f9c8
Moises <moises.segura94@gmail.com>
2023-04-20 18:36:45 -0400

    Initial commit

---
+++ app/src/main/java/bootcamp/cl/ejemplo/appveterinarioperruno/VerFichaActivity.java
@@ -0,0 +1,14 @@
+package bootcamp.cl.ejemplo.appveterinarioperruno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerFichaActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
